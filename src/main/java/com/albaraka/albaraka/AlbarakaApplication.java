cb3d6df963935798166c66d01758e3ee58fd27ac
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2025-12-16 13:21:33 +0100

    ABD-3 #done: Initialiser le projet utilisant Spring Initializr

---
+++ src/main/java/com/albaraka/albaraka/AlbarakaApplication.java
@@ -0,0 +1,13 @@
+package com.albaraka.albaraka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbarakaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbarakaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/albaraka/albaraka/AlbarakaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albaraka.albaraka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbarakaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
