8c5b5fcb3dd5bcead03f2c2aa24a81ebe9c28eec
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2025-12-17 13:00:23 +0100

    ABD-20 #done: Créer l’entité Role et User

---
+++ src/main/java/com/albaraka/albaraka/model/entity/Role.java
@@ -0,0 +1,9 @@
+package com.albaraka.albaraka.model.entity;
+
+import java.util.UUID;
+
+public class Role {
+    private Long id;
+    private UUID uuid;
+    private String name;
+}
--- src/main/java/com/albaraka/albaraka/model/entity/User.java
+++ src/main/java/com/albaraka/albaraka/model/entity/User.java
@@ -1,4 +1,16 @@
 package com.albaraka.albaraka.model.entity;
 
+import com.albaraka.albaraka.model.enums.UserStatus;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 public class User {
+    private Long id;
+    private UUID uuid;
+    private String fullname;
+    private String email;
+    private String password;
+    private UserStatus status;
+    private LocalDateTime createdAt;
 }
