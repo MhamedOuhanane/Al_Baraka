992bfb1f8dfa7f46cc28b354cea3843ef8113841
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2025-12-18 00:58:25 +0100

    ABD-37 #done: Ajouter validations Operation DTO

--- src/main/java/com/albaraka/albaraka/model/dto/operation/OperationCreateDTO.java
+++ src/main/java/com/albaraka/albaraka/model/dto/operation/OperationCreateDTO.java
@@ -4,6 +4,8 @@ import com.albaraka.albaraka.model.dto.account.AccountDTO;
 import com.albaraka.albaraka.model.dto.document.DocumentDTO;
 import com.albaraka.albaraka.model.enums.OperationStatus;
 import com.albaraka.albaraka.model.enums.OperationType;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -17,8 +19,14 @@ import java.util.UUID;
 @AllArgsConstructor
 @EqualsAndHashCode
 public class OperationCreateDTO {
+
+    @NotNull(message = "Le type d'opération est obligatoire")
     private OperationType type;
+
+    @NotNull(message = "Le montant est obligatoire")
+    @Positive(message = "Le montant doit être positif")
     private BigDecimal amount;
+
     private UUID accountSourceUuid;
     private UUID accountDestinationUuid;
     private UUID documentUuid;
