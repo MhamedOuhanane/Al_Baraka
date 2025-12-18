74b11114d8d1d19fc2a5e3d93a85542e27fcfc0c
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2025-12-18 23:20:22 +0100

    ABD-46 #done: Créer l'interface UseRepository

--- src/main/java/com/albaraka/albaraka/repository/UserRepository.java
+++ src/main/java/com/albaraka/albaraka/repository/UserRepository.java
@@ -1,4 +1,15 @@
 package com.albaraka.albaraka.repository;
 
-public interface UserRepository {
+import com.albaraka.albaraka.model.entity.Role;
+import com.albaraka.albaraka.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByUuid(UUID uuid);
+    Optional<User> findByEmail(String email);
+    List<User> findByRole(Role role);
 }
