d70eefe41b3ed6e1bcb6b37fc757a6dcfbc60d9a
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2026-01-15 10:21:29 +0100

    Ajouter CI/CD pipeline

---
+++ src/main/java/com/albaraka/albaraka/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.albaraka.albaraka.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Value("${app.frontend.url}")
+    private String frontendUrl;
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin(frontendUrl);
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
